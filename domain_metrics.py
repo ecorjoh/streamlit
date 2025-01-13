6d9a4bee3f50d625917ae53cad2d6792d400baf8
ecorjoh <166039786+ecorjoh@users.noreply.github.com>
2025-01-13 14:18:10 -0600

    Update domain_metrics.py

--- domain_metrics.py
+++ domain_metrics.py
@@ -88,7 +88,7 @@ def main():
             specs=[
                 [{"type": "xy"}, {"type": "domain"}]
             ],
-            horizontal_spacing=0.09,  # space between the two columns
+            horizontal_spacing=0.1,  # space between the two columns
             column_widths=[0.6, 0.4], # adjust widths: 60% for scatter, 40% for table
             subplot_titles=("Opportunity Scatter", "Data Table")
         )
